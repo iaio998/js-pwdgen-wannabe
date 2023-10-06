177143f49c5f29fa501f59f258ca7b52c6c1f302
iaio998 <depalatisiacopo@gmail.com>
2023-10-06 14:47:15 +0200

    working script

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,17 @@
 // Chiedi all’utente il suo nome
+let nome = prompt("Inserisci il tuo nome.");
+
 // poi chiedi il suo cognome
+let cognome = prompt("Inserisci il tuo cognomem, senza spazi se presenti.");
+
 // poi chiedi il suo colore preferito
+let favCol = prompt("Inserisci il tuo colore preferito.");
+
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
+const numero = 21;
+
+document.getElementById("password").innerHTML = `
+<h2>
+${nome}${cognome}${favCol}${numero}
+    </h2>
+`;
